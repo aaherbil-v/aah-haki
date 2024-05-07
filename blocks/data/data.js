dccce103874f381eae41528ca3665adff8b8a25c
Abdellah Aherbil <abdellah.aherbil@valtech.com>
2024-05-07 13:29:16 +0200

    update build

--- blocks/data/data.js
+++ blocks/data/data.js
@@ -1,25 +1,16 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
-export default function decorate(block) {
-  /* change to ul, li */
-  const div = document.createElement('div');
-  fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
+const textContent = await fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
     return response.json();
   })
-  .then(data => {
-    console.log(data);
-    block.textContent = data[0].title;
-    // Do something with the JSON data
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+  .then((data) => data[0].title)
+  .catch(() => 'There was a problem with the fetch operation');
 
-  
+export default function decorate(block) {
+  /* change to ul, li */
+  const div = document.createElement('div');
+  div.textContent = textContent;
   block.append(div);
 }
